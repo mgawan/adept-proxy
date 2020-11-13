0af5dde9c525c4d1cb1a2352120e4d829f513f7f
Muaaz Awan <mgawan@lbl.gov>
2020-11-13 15:24:31 -0600

    some tests changed, some cmakelist changes

--- adept-cuda/quick_regression_test.sh
+++ adept-cuda/quick_regression_test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-srun ./program_gpu dna ../test-data/dna-reference.fasta     ../test-data/dna-query.fasta     test_regression_dna_out     >/dev/null 2>&1
-srun ./program_gpu aa  ../test-data/protein-reference.fasta ../test-data/protein-query.fasta test_regression_protein_out >/dev/null 2>&1
+srun ./program_gpu dna ../../test-data/dna-reference.fasta     ../../test-data/dna-query.fasta     test_regression_dna_out     >/dev/null 2>&1
+srun ./program_gpu aa  ../../test-data/protein-reference.fasta ../../test-data/protein-query.fasta test_regression_protein_out >/dev/null 2>&1
 
-diff_dna=$(diff -b test_regression_dna_out     ../test-data/results_dna)
-diff_aa=$(diff -b test_regression_protein_out ../test-data/results_aa)
+diff_dna=$(diff -b test_regression_dna_out     ../../test-data/results_dna)
+diff_aa=$(diff -b test_regression_protein_out ../../test-data/results_aa)
 
 if [ "$diff_dna" == "" ] && [ "$diff_aa" == "" ]
 then
